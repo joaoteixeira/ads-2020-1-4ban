ea46cf602b7ae039482408b32a131d6cc1bd6dbd
Eli Gabriel <eligabriel2013@gmail.com>
2020-06-06 23:05:36 -0300

    Troca de SGBD, triggers e views adicionadas
    criação - trocar o tipo de chave primaria de todas as tabelas, de serial(big int(20)), para integer auto incremment. nomes de chaves estrangeiras foram simplificados
    inserções - reajuste no formato das datas
    triggers - adicionados triggers que possibilitam a exclusão de dados que tenham dependência em outra tabela
    views - algumas views que vão facilitar a busca e exibição de postagems

--- database/4ban_script_criacao.sql
+++ database/4ban_script_criacao.sql
@@ -5,13 +5,14 @@ use 4ban;
 */
 
 create table Tag (
-tag_id serial not null,
+tag_id integer AUTO_INCREMENT not null,
 tag_nome varchar (15) not null,
 tag_descricao varchar (255),
 primary key(tag_id)
 );
+
 create table Usuario(
-usuario_id serial not null,
+usuario_id integer AUTO_INCREMENT not null,
 usuario_email varchar(255),
 usuario_login varchar(255),
 usuario_senha varchar(255),
@@ -22,36 +23,36 @@ primary key (usuario_id)
 );
 
 create table Postagem(
-postagem_id serial not null,
+postagem_id integer AUTO_INCREMENT not null,
 postagem_titulo varchar(255),
 postagem_up int,
 postagem_down int,
 postagem_data_publicacao date,
 postagem_texto varchar(500),
-fk_Usuario_ususario_id int,
+fk_usuario_id int,
 primary key (postagem_id),
-foreign key (fk_Usuario_ususario_id) references Usuario(usuario_id)
+foreign key (fk_usuario_id) references Usuario(usuario_id)
 );
 /*talvez coloque o campo para imagem*/
 
 create table Comentario(
-comentario_id serial not null,
+comentario_id integer AUTO_INCREMENT not null,
 comentario_data_publicacao date,
 comentario_texto varchar(500),
 comentario_fixado boolean,
-fk_Comentario_comentario_id int,
-fk_Postagem_postagem_id int,
-fk_Usuario_usuario_id int,
+fk_comentario_id int,
+fk_postagem_id int,
+fk_usuario_id int,
 primary key (comentario_id),
-foreign key (fk_Comentario_comentario_id) references Comentario(comentario_id),
-foreign key (fk_Postagem_postagem_id) references Postagem(postagem_id),
-foreign key (fk_Usuario_usuario_id) references Usuario(usuario_id)
+foreign key (fk_comentario_id) references Comentario(comentario_id),
+foreign key (fk_postagem_id) references Postagem(postagem_id),
+foreign key (fk_usuario_id) references Usuario(usuario_id)
 );
 
 create table tag_has_postagem(
-fk_Tag_tag_id int not null, 
-fk_Postagem_postagem_id int not null,
-primary key(fk_Tag_tag_id, fk_Postagem_postagem_id),
-foreign key (fk_Tag_tag_id) references Tag(tag_id),
-foreign key (fk_Postagem_postagem_id) references Postagem(postagem_id)
+fk_tag_id int not null, 
+fk_postagem_id int not null,
+primary key(fk_tag_id, fk_postagem_id),
+foreign key (fk_tag_id) references Tag(tag_id),
+foreign key (fk_postagem_id) references Postagem(postagem_id)
 );
