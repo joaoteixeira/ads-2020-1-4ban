9eaec8060d5c67a7dafe82b96ef860564b455912
Eli Gabriel <eligabriel2013@gmail.com>
2020-06-10 00:57:12 -0300

    Layout básico de postagem
    Commit rápido apenas com a formação inicial que as postagens irão ter.
    A div com os comentários é "temporária".

--- resources/views/postagem/show.blade.php
+++ resources/views/postagem/show.blade.php
@@ -3,17 +3,26 @@
 @section('content-title', 'Postagens')
 
 @section('content')
-    <div class="mb-3">
-        <div>
-            <p>{{ $postagem->postagem_up }}</p>
-            <p>{{ $postagem->postagem_down }}</p>
-        </div>
-        <div>
+    <div class="card mt-4">
+        <div class="card-body">
             <h3>{{ $postagem->postagem_titulo }}</h3>
-        </div>
-        <div>
             <p>{{ $postagem->postagem_data_publicacao }}</p>
             <p>{{ $postagem->postagem_texto }}</p>
+        </div>
+
+        <div class="card card-outline-secondary my-4">
+            <div class="card-header">Comentários</div>
+            <div class="card-body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+            <small class="text-muted">Posted by Anonymous on 3/1/17</small>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+            <small class="text-muted">Posted by Anonymous on 3/1/17</small>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+            <small class="text-muted">Posted by Anonymous on 3/1/17</small>
+            <hr>
+            </div>
         </div>
     </div>
 @endsection()
